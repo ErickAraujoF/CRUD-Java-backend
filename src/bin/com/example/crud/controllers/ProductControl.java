afcb1cea0ddcbf7fc19c91b22e9860407199a0a5
Erick Fagundes Araújo <105995262+ErickAraujoF@users.noreply.github.com>
2023-08-09 16:37:05 -0300

    começando

---
+++ src/bin/com/example/crud/controllers/ProductControl.java
@@ -0,0 +1,21 @@
+package com.example.crud.controllers
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductControl {
+   @GetMapping
+   public ResponseEntity getAllProduct() {
+	   return ResponseEntity.ok(body: "Ok");
+	   
+   }
+   
+
+
+
+
+}
